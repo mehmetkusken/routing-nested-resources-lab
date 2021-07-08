22fc2d32cb65660fffc4532eb5d677f52b15982f
Mehmet <mehmetkusken@gmail.com>
2021-07-08 14:08:46 -0600

    commit tree ef4b073342cc3790bf9647c839fbf6be6743a942

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,11 @@
 module SongsHelper
+    def artist_select(form_item, song)
+        if song.artist.nil?
+          form_item.label :artist_name
+          select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+        else
+          hidden_field_tag "song[artist_id]", song.artist_id
+        end
+      end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   resources :artists
+
   resources :songs
+
+
+  resources :artists do
+    resources :songs, only: [:index, :show]
+
+  end
+
+
+
 end
